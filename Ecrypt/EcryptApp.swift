73a56826b81795cf614f8ff4dfd74ab30c79f41b
NorthDevCO <167841713+NorthDevCO@users.noreply.github.com>
2024-04-23 22:37:40 +1000

    Initial Commit

---
+++ Ecrypt/EcryptApp.swift
@@ -0,0 +1,17 @@
+//
+//  EcryptApp.swift
+//  Ecrypt
+//
+//  Created by Liam Fletcher on 23/4/2024.
+//
+
+import SwiftUI
+
+@main
+struct EcryptApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
