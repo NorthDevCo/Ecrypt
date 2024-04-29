bc865a94b27ba01f875fcb02bdbd3c11588004c0
NorthDevCO <167841713+NorthDevCO@users.noreply.github.com>
2024-04-29 21:00:56 +1000

    Reorganisation of user sign up views hopefully this is going to work but i don't know trying to create an introduction for user before registration

---
+++ Ecrypt/Views/SettingsView.swift
@@ -0,0 +1,18 @@
+//
+//  SystemSettingsView.swift
+//  Ecrypt
+//
+//  Created by Liam Fletcher on 29/4/2024.
+//
+
+import SwiftUI
+
+struct SystemSettingsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SystemSettingsView()
+}
